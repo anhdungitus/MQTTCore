d2afc034b569fab0b0494ab89f3db8723a0d22eb
Dung Nguyen <Dung.Nguyen@hansencx.com>
2020-01-30 09:57:18 +0700

    Add project files.

---
+++ MQTTTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQTTTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
